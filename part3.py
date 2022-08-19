253a77ac1eac81b7216cc3fe3eca3e794257b71b
blarblar <mm11@umbc.edu>
2022-08-19 11:03:25 -0700

    Create part3.py

---
+++ part3.py
@@ -0,0 +1,21 @@
+'''
+Given a tic tac toe board stored in a 2d list, write a function that returns 1 if X has won, -1 if Y has won, and 0
+if no one has won.
+
+For example, the board
+
+X O X
+_ _ _
+O _ X
+
+Would be represented as:
+
+[ [1, -1, -1], [0, 0, 0], [-1, 0, 1] ]
+
+'''
+
+def checkWin(board):
+  # Your code here!
+  
+board = [ [1, -1, -1], [0, 0, 0], [-1, 0, 1] ]
+checkWin(board)

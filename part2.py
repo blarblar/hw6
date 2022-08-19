7195e6600ef3e21ed8a5573e7c2730ab2a918dec
blarblar <mm11@umbc.edu>
2022-08-19 10:59:06 -0700

    Create part2.py

---
+++ part2.py
@@ -0,0 +1,11 @@
+'''
+A csv file is a file representing a table where each line is a row, and columns are separated by commas.
+For example, 
+
+Max,34,Male
+Timmy,7,Male
+
+Write a program that reads data.csv (also in this directory) and writes only the second column to a file. 
+You may need the split function: https://www.w3schools.com/python/ref_string_split.asp
+
+'''

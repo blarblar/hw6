2622865e7969cc562cadced52e32199629c7cf80
blarblar <mm11@umbc.edu>
2022-08-19 11:00:35 -0700

    Update part2.py

--- part2.py
+++ part2.py
@@ -5,7 +5,7 @@ For example,
 Max,34,Male
 Timmy,7,Male
 
-Write a program that reads data.csv (also in this directory) and writes only the second column to a file. 
+Write a program that reads data.csv (also in this directory) and writes only the second column to a file, each number on its own line. 
 You may need the split function: https://www.w3schools.com/python/ref_string_split.asp
 
 '''

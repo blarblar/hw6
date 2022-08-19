b01bacb729f333e7848053ced0043b19673e60c2
blarblar <mm11@umbc.edu>
2022-08-19 10:55:44 -0700

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,10 @@
+# Write a function that takes a list of integers and sorts it!
+
+# This may be difficult. Keep your instructions to Timmy clearly in your head! If possible,
+# break your program up into sub-parts that are easier to think about
+
+def mySort(testlist):
+  # Your code here!
+
+testList = [ 1, 4, 5, 0, 3, 8, 100, 17, 12, 56 ] # Leave this here for my testing
+print(mySort(testlist)) 
